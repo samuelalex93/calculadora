ac42c8ccbdd2f804c2a01e3844717443ce2a9301
Samuel Aguiar <muka_gt@hotmail.com>
2021-06-16 15:31:17 -0300

    Refatoração de calculadora

--- src/js/calculadora.js
+++ src/js/calculadora.js
@@ -1,44 +1,59 @@
 var Calculadora = {
-  adicionar: function(num1, num2){
-    if(isNaN(num1) || isNaN(num2)) {
-      return 0;
-    }
-    num1 = parseFloat(num1);
-    num2 = parseFloat(num2);
-    return num1 + num2;
+  ADICAO : '+',
+  MULTIPLICACAO : '*',
+  SUBTRACAO : '-',
+  DIVISAO : '/',
+
+  adicionar: function(num1, num2){   
+    return Calculadora.calcular(num1, num2, Calculadora.ADICAO);
   },
 
   subtrair: function(num1, num2){
-    if(isNaN(num1) || isNaN(num2)) {
-      return 0;
-    }
-    num1 = parseFloat(num1);
-    num2 = parseFloat(num2);
-    return num1 - num2;
+    return Calculadora.calcular(num1, num2, Calculadora.SUBTRACAO);
   },
 
   dividir: function(num1, num2){
-    if((isNaN(num1) || isNaN(num2))) {
-      return 0;
-    }
-
     if(num1 == 0 || num2 == 0) {
       return 'Erro';
     }
-
-    num1 = parseFloat(num1);
-    num2 = parseFloat(num2);
-    return num1 / num2;
+    return Calculadora.calcular(num1, num2, Calculadora.DIVISAO);
   },
 
   multiplicar: function(num1, num2){
+    return Calculadora.calcular(num1, num2, Calculadora.MULTIPLICACAO);
+  },
+
+  calcular: function(num1, num2, operacao) {
+    var result = 0;
+
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+
     if(isNaN(num1) || isNaN(num2)) {
       return 0;
     }
-    num1 = parseFloat(num1);
-    num2 = parseFloat(num2);
-    return num1 * num2;
-  }
+
+    switch (operacao) {
+      case Calculadora.ADICAO:
+        result = num1 + num2;
+        break;
+      case Calculadora.SUBTRACAO:
+        result = num1 - num2;
+        break;
+      case Calculadora.MULTIPLICACAO:
+        result = num1 * num2;
+        break;
+      case Calculadora.DIVISAO:
+        result = num1 / num2;
+        break;
+      default:
+        result = 0;
+        break
+    }
+
+    return result;
+
+  },
 }
 
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
